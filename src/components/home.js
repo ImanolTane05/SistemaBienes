fd11495a6098b8440411ff835a6096e83a926a9d
Imanol Sánchez <tane05ima@gmail.com>
2024-07-11 22:44:07 -0600

    mostrar datos

--- src/components/home.js
+++ src/components/home.js
@@ -1,43 +1,60 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import appfirebase from '../credenciales'
 import { getAuth, signOut } from 'firebase/auth'
-import {getFirestore,collection,addDoc,getDocs,doc,deleteDoc,getDoc,setDoc} from 'firebase/firestore'
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc } from 'firebase/firestore'
 
 const auth = getAuth(appfirebase)
-
 const db = getFirestore(appfirebase)
 
 const Home = ({ correoUsuario }) => {
-
     const valorInicial = {
-        nombre:'',
-        edad:'',
-        profesion:''
+        nombre: '',
+        edad: '',
+        profesion: ''
     }
 
-    const [user,setUser]=useState(valorInicial)
+    // VARIABLES DE ESTADO
+    const [user, setUser] = useState(valorInicial)
+    const [lista, setLista] = useState([])
 
-    const capturarInputs =(e) =>{
-        const {name,value}=e.target;
-        setUser({...user,[name]:value})
+    // FUNCION PARA CAPTURAR INPUTS
+    const capturarInputs = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value })
     }
 
-    const guardardatos =async (e)=>{
+    const guardardatos = async (e) => {
         e.preventDefault();
-        //console.log(user);
         try {
-            await addDoc(collection(db,'usuarios'),{
+            await addDoc(collection(db, 'usuarios'), {
                 ...user
             })
         } catch (error) {
             console.log(error);
         }
-        setUser({...valorInicial})
+        setUser({ ...valorInicial })
     }
+
+    // Funcion para renderizar la lista de usuarios
+    useEffect(() => {
+        const getLista = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'usuarios'))
+                const docs = []
+                querySnapshot.forEach((doc) => {
+                    docs.push({ ...doc.data(), id: doc.id })
+                })
+                setLista(docs)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getLista()
+    }, [])
 
     return (
         <div className='container'>
-            <p>Bienvenido,<strong>{correoUsuario}</strong> Haz iniciado sesión</p>
+            <p>Bienvenido, <strong>{correoUsuario}</strong> Haz iniciado sesión</p>
 
             <button className='btn btn-primary' onClick={() => signOut(auth)}>
                 Cerrar Sesión
@@ -45,21 +62,20 @@ const Home = ({ correoUsuario }) => {
 
             <hr />
             <div className='row'>
-                {/* esta seccion sera el formulario */}
+                {/* Esta sección será el formulario */}
                 <div className="col-md-4">
                     <h3 className='text-center mb-3'>Ingresa Usuarios</h3>
                     <form onSubmit={guardardatos}>
                         <div className='card card-body'>
                             <div className='form-group'>
-                                <input type='text' name='nombre' className='form-control mb-3' placeholder='ingresar el nombre del usuario' 
-                                onChange={capturarInputs} value={user.nombre} required/>
+                                <input type='text' name='nombre' className='form-control mb-3' placeholder='Ingresar el nombre del usuario'
+                                    onChange={capturarInputs} value={user.nombre} required />
 
                                 <input type='text' name='edad' className='form-control mb-3' placeholder='Ingresa la edad '
-                                onChange={capturarInputs} value={user.edad} required/>
+                                    onChange={capturarInputs} value={user.edad} required />
 
-                                <input type='text' name='profesion' className='form-control mb-3' placeholder='Ingresa la porfesion'
-                                onChange={capturarInputs} value={user.profesion} required/>
-
+                                <input type='text' name='profesion' className='form-control mb-3' placeholder='Ingresa la profesion'
+                                    onChange={capturarInputs} value={user.profesion} required />
                             </div>
                             <button className='btn btn-primary'>
                                 Guardar
@@ -67,9 +83,31 @@ const Home = ({ correoUsuario }) => {
                         </div>
                     </form>
                 </div>
-                {/* esta seccion sera la lista de nuestros usuarios */}
+                {/* Esta sección será la lista de nuestros usuarios */}
                 <div className="col-md-8">
                     <h2 className='text-center mb-5'>Lista de Usuarios</h2>
+                    <div className='container card'>
+                        <div className='card-body'>
+                            {
+                                lista.map(list => (
+                                    <div key={list.id}>
+                                        <p>Nombre: {list.nombre}</p>
+                                        <p>Edad: {list.edad}</p>
+                                        <p>Profesion: {list.profesion}</p>
+
+                                        <button className='btn btn-danger'>
+                                            Eliminar
+                                        </button>
+
+                                        <button className='btn btn-success m-1'>
+                                            Actualizar
+                                        </button>
+                                        <hr />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
